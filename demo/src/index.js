36543e6011ebe283c589a0c064aa9de9a4d711bb
Laza Bogdan <laza.bogdan@macro-net.ro>
2016-02-16 23:27:17 +0200

    update demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -4,12 +4,18 @@ window.$ = jQuery
 
 // AdminPlus Style
 import 'adminplus/dist/adminplus.css'
+import './css/_docs.css'
 
 // HIGHLIGHT.JS
 import 'highlight.js/styles/github-gist.css'
 
+import Vue from 'vue'
+
 // Router requires a component to start
-import Main from './views/main'
+// import Main from './views/main'
+const Main = Vue.extend({
+	template: '<router-view></router-view>'
+})
 
 // Vue router
 import router from './router'
--- demo/src/views/docs/layout.vue
+++ demo/src/views/docs/layout.vue
@@ -2,11 +2,11 @@
 	<!-- Navbar -->
 	<nav class="navbar navbar-dark bg-primary navbar-full navbar-fixed-top">
 		<div class="container">
-			<a class="navbar-brand m-r-2" href="#">Introduction</a>
+			<a class="navbar-brand m-r-2" href="#">AdminPlus Docs</a>
 	  		<div class="collapse navbar-toggleable-sm navbar-toggleable-md" id="navbarMenu">
 				<ul class="nav navbar-nav">
 					<li class="nav-item active">
-						<a class="nav-link" href="#overview">Overview</a>
+						<a class="nav-link" href="#overview">Introduction</a>
 					</li>
 					<li class="nav-item">
 						<a class="nav-link" href="#structure">Structure</a>
